759b9a9d83f0be2cb3fb8d9b019999f20232733d
Bruno Martin <bruno.martin@hacklab.com.br>
2015-05-12 22:53:34 -0300

    update migrations

--- mapaguarani/core/migrations/0001_initial.py
+++ mapaguarani/core/migrations/0001_initial.py
@@ -14,43 +14,43 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='ArchaeologicalPlace',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
-                ('name', models.CharField(max_length=255, verbose_name='name')),
-                ('acronym', models.CharField(max_length=512, verbose_name='name')),
-                ('position_precision', models.CharField(choices=[('exact', 'Exact'), ('estimated', 'Estimated'), ('by_city', 'By City'), ('no_position', 'No position')], max_length=128, verbose_name='Position Precision')),
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
+                ('name', models.CharField(verbose_name='name', max_length=255)),
+                ('acronym', models.CharField(verbose_name='name', max_length=512)),
+                ('position_precision', models.CharField(verbose_name='Position Precision', max_length=128, choices=[('exact', 'Exact'), ('estimated', 'Estimated'), ('by_city', 'By City'), ('no_position', 'No position')])),
                 ('position_comments', models.TextField(verbose_name='Position Comments')),
             ],
         ),
         migrations.CreateModel(
             name='Documentation',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
                 ('file', models.FileField(upload_to='', verbose_name='attached file')),
                 ('date', models.DateField(verbose_name='Date')),
-                ('description', models.TextField(blank=True, null=True, verbose_name='description')),
+                ('description', models.TextField(null=True, blank=True, verbose_name='description')),
             ],
         ),
         migrations.CreateModel(
             name='DocumentationType',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
-                ('name', models.CharField(max_length=255, verbose_name='name')),
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
+                ('name', models.CharField(verbose_name='name', max_length=255)),
             ],
         ),
         migrations.CreateModel(
             name='EthnicGroup',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
-                ('name', models.CharField(max_length=255, verbose_name='name')),
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
+                ('name', models.CharField(verbose_name='name', max_length=255)),
             ],
         ),
         migrations.CreateModel(
             name='GuaraniPresence',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
                 ('presence', models.BooleanField(verbose_name='Guarani presence')),
                 ('date', models.DateField(verbose_name='Date')),
-                ('source', models.CharField(max_length=512, verbose_name='name')),
+                ('source', models.CharField(verbose_name='name', max_length=512)),
             ],
             options={
                 'get_latest_by': 'date',
@@ -59,20 +59,20 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='IndigenousLand',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
-                ('name', models.CharField(max_length=255, verbose_name='name')),
-                ('other_names', models.CharField(max_length=512, verbose_name='Others names')),
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
+                ('name', models.CharField(verbose_name='name', max_length=255)),
+                ('other_names', models.CharField(verbose_name='Others names', max_length=512)),
                 ('official_area', models.FloatField(verbose_name='Official area')),
-                ('guarani_exclusive_possession_area_portion', models.FloatField(blank=True, null=True, verbose_name='Guarani full and exclusive portion area possession')),
-                ('others_exclusive_possession_area_portion', models.FloatField(blank=True, null=True, verbose_name='Others people full and exclusive portion area possession')),
-                ('claim', models.TextField(blank=True, null=True, verbose_name='Clain')),
-                ('demand', models.TextField(blank=True, null=True, verbose_name='Demand')),
-                ('source', models.CharField(max_length=512, verbose_name='Source')),
-                ('public_comments', models.TextField(blank=True, null=True, verbose_name='Comments')),
-                ('private_comments', models.TextField(blank=True, null=True, verbose_name='Private comments')),
+                ('guarani_exclusive_possession_area_portion', models.FloatField(null=True, blank=True, verbose_name='Guarani full and exclusive portion area possession')),
+                ('others_exclusive_possession_area_portion', models.FloatField(null=True, blank=True, verbose_name='Others people full and exclusive portion area possession')),
+                ('claim', models.TextField(null=True, blank=True, verbose_name='Clain')),
+                ('demand', models.TextField(null=True, blank=True, verbose_name='Demand')),
+                ('source', models.CharField(verbose_name='Source', max_length=512)),
+                ('public_comments', models.TextField(null=True, blank=True, verbose_name='Comments')),
+                ('private_comments', models.TextField(null=True, blank=True, verbose_name='Private comments')),
                 ('polygon', django.contrib.gis.db.models.fields.PolygonField(srid=4326)),
                 ('documentation', models.ManyToManyField(to='core.Documentation', verbose_name='documentation')),
-                ('ethnic_groups', models.ManyToManyField(related_name='indigenousland_ethnic_groups_layers', to='core.EthnicGroup', verbose_name='Ethnic group')),
+                ('ethnic_groups', models.ManyToManyField(to='core.EthnicGroup', verbose_name='Ethnic group', related_name='indigenousland_ethnic_groups_layers')),
             ],
             options={
                 'abstract': False,
@@ -81,17 +81,17 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='IndigenousVillage',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
-                ('name', models.CharField(max_length=255, verbose_name='name')),
-                ('other_names', models.CharField(max_length=512, verbose_name='Others names')),
-                ('comments', models.TextField(blank=True, null=True, verbose_name='Comments')),
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
+                ('name', models.CharField(verbose_name='name', max_length=255)),
+                ('other_names', models.CharField(verbose_name='Others names', max_length=512)),
+                ('comments', models.TextField(null=True, blank=True, verbose_name='Comments')),
                 ('position', django.contrib.gis.db.models.fields.PointField(srid=4326)),
-                ('population', models.CharField(blank=True, null=True, max_length=512)),
-                ('guarani_presence', models.CharField(blank=True, null=True, max_length=512)),
-                ('prominent_subgroup', models.CharField(blank=True, null=True, max_length=512)),
-                ('ethnic_groups2', models.CharField(blank=True, null=True, max_length=512, verbose_name='Ethnic group')),
+                ('population', models.CharField(null=True, blank=True, max_length=512)),
+                ('guarani_presence', models.CharField(null=True, blank=True, max_length=512)),
+                ('prominent_subgroup', models.CharField(null=True, blank=True, max_length=512)),
+                ('ethnic_groups2', models.CharField(null=True, blank=True, verbose_name='Ethnic group', max_length=512)),
                 ('documentation', models.ManyToManyField(to='core.Documentation', verbose_name='documentation')),
-                ('ethnic_groups', models.ManyToManyField(related_name='indigenousvillage_ethnic_groups_layers', to='core.EthnicGroup', verbose_name='Ethnic group')),
+                ('ethnic_groups', models.ManyToManyField(to='core.EthnicGroup', verbose_name='Ethnic group', related_name='indigenousvillage_ethnic_groups_layers')),
             ],
             options={
                 'abstract': False,
@@ -100,8 +100,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='LegalProceedings',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
-                ('name', models.CharField(max_length=255, verbose_name='name')),
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
+                ('name', models.CharField(verbose_name='name', max_length=255)),
                 ('description', models.TextField(verbose_name='Description')),
                 ('indigenous_land', models.ForeignKey(to='core.IndigenousLand', verbose_name='Guarani indigenous lands layer')),
                 ('indigenous_village', models.ForeignKey(to='core.IndigenousVillage', verbose_name='Guarani indigenous villages layer')),
@@ -110,19 +110,19 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='MapLayer',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
-                ('name', models.CharField(max_length=255, verbose_name='name')),
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
+                ('name', models.CharField(verbose_name='name', max_length=255)),
                 ('description', models.TextField(verbose_name='Description')),
             ],
         ),
         migrations.CreateModel(
             name='Population',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
                 ('population', models.IntegerField(verbose_name='Population')),
                 ('date', models.DateField(verbose_name='Date')),
-                ('source', models.CharField(max_length=512, verbose_name='name')),
-                ('village', models.ForeignKey(related_name='population_annual_series_population', to='core.IndigenousVillage', verbose_name='Village')),
+                ('source', models.CharField(verbose_name='name', max_length=512)),
+                ('village', models.ForeignKey(verbose_name='Village', related_name='population_annual_series_population', to='core.IndigenousVillage')),
             ],
             options={
                 'get_latest_by': 'date',
@@ -131,12 +131,12 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='indigenousvillage',
             name='layer',
-            field=models.ForeignKey(blank=True, related_name='villages', null=True, to='core.MapLayer', verbose_name='Layer'),
+            field=models.ForeignKey(null=True, blank=True, verbose_name='Layer', related_name='villages', to='core.MapLayer'),
         ),
         migrations.AddField(
             model_name='indigenousland',
             name='layer',
-            field=models.ForeignKey(related_name='indigenous_lads', to='core.MapLayer', verbose_name='Layer'),
+            field=models.ForeignKey(verbose_name='Layer', related_name='indigenous_lads', to='core.MapLayer'),
         ),
         migrations.AddField(
             model_name='guaranipresence',
@@ -151,6 +151,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='archaeologicalplace',
             name='layer',
-            field=models.ForeignKey(related_name='archaeological_places', to='core.MapLayer', verbose_name='Layer'),
+            field=models.ForeignKey(verbose_name='Layer', related_name='archaeological_places', to='core.MapLayer'),
         ),
     ]
