099290fc57ef788341f17a23da9e9b3bad621093
Bruno Martin <bruno.martin@hacklab.com.br>
2015-07-20 02:17:50 -0300

    refatorado legendas e definição de cores no mapas

--- mapaguarani/static/js/controllers.js
+++ mapaguarani/static/js/controllers.js
@@ -9,7 +9,7 @@
         function ($scope, IndigenousVillage, IndigenousLands) {
 
             var map = L.map('map', {
-                center: [-23.5408, -46.6400],
+                center: [-16.107747, -51.103348],
                 zoom: 5
             });
 
@@ -66,18 +66,129 @@
                 layers_control.addOverlay(markers, 'Aldeias Indígenas');
             });
 
+            var land_tenures = {
+                'no_arrangements': {
+                    name: 'Sem Providências',
+                    style: {
+                        color: '#ffffff',
+                        fillColor: '#ffffff',
+                        dashArray: '5'
+                    }
+                },
+                'regularized': {
+                    name: 'Regularizada',
+                    style: {
+                        color: '#009933',
+                        fillColor: '#33AD5C'
+                    }
+                },
+                'expropriated': {
+                    name: 'Desapropriada',
+                    style: {
+                        color: '#00E600',
+                        fillColor: '#00FF00',
+                        dashArray: '5'
+                    }
+                },
+                'expropriated_in_progress': {
+                    name: 'Em processo de desapropriação',
+                    style: {
+                        color: '#4DFF4D',
+                        fillColor: '#66FF66',
+                        dashArray: '5'
+                    }
+                },
+                'delimited': {
+                    name: 'Delimitada',
+                    style: {
+                        color: '#00FF99',
+                        fillColor: '#33FFAD',
+                        dashArray: '5'
+                    }
+                },
+                'study': {
+                    name: 'Em estudo',
+                    style: {
+                        color: '#FF3300',
+                        fillColor: '#FF4719',
+                        dashArray: '5'
+                    }
+                },
+                'declared': {
+                    name: 'Declarada',
+                    style: {
+                        color: '#CC9900',
+                        fillColor: '#D6AD33'
+                    }
+                },
+                'acquired': {
+                    name: 'Adquirida',
+                    style: {
+                        color: '#CC9900',
+                        fillColor: '#D6AD33'
+                    }
+                },
+                'regularized_limits_rev': {
+                    name: 'Regularizada (Em revisão de limites)',
+                    style: {
+                        color: '#EBD699',
+                        fillColor: '#F5EBCC'
+                    }
+                }
+            };
+
+            var legend_control = L.control({position: 'bottomright'});
+            legend_control.onAdd = function (map) {
+                var div = L.DomUtil.create('div', 'info legend');
+                var tenure;
+                div.innerHTML += '<p><strong>Terras indígenas</strong></p>';
+                for (tenure in land_tenures) {
+                    div.innerHTML +=
+                        '<i style="background:' + land_tenures[tenure].style.fillColor + '"></i> ' +
+                        land_tenures[tenure].name + '<br>';
+                }
+                return div;
+            };
+
             IndigenousLands.get({}, function(lands){
                 var indigenous_lands_layer = L.geoJson(lands, {
-                    //style: function (feature) {
-                    //    return {color: feature.properties.color};
-                    //},
-                    //onEachFeature: function (feature, layer) {
-                    //    layer.bindPopup(feature.properties.description);
-                    //}
+                    style: function (feature) {
+                        var style = {};
+                        var land_tenure = land_tenures[feature.properties.land_tenure];
+                        if (land_tenure) {
+                            style = land_tenure.style;
+                        }
+                        style.opacity = 0.8;
+                        style.fillOpacity = 0.5;
+                        style.weight = 2
+                        return style;
+                    },
+                    onEachFeature: function (feature, layer) {
+                        var land_tenure = land_tenures[feature.properties.land_tenure];
+                        if (land_tenure)
+                            layer.bindPopup(land_tenure.name);
+                        else
+                            layer.bindPopup(feature.properties.land_tenure);
+                    }
                 });
 
                 map.addLayer(indigenous_lands_layer);
                 layers_control.addOverlay(indigenous_lands_layer, 'Terras indígenas');
+
+                map.on('overlayadd', function (eventLayer) {
+                    // Switch to the Population legend...
+                    if (eventLayer.name === 'Terras indígenas') {
+                        legend_control.addTo(this);
+                    }
+                });
+                map.on('overlayremove', function (eventLayer) {
+                    if (eventLayer.name === 'Terras indígenas') {
+                        this.removeControl(legend_control);
+                    }
+                });
+
+                legend_control.addTo(map);
+                //var sidebar = L.control.sidebar('sidebar').addTo(map);
             });
         }
     ]);
