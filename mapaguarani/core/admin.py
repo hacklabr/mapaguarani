d8669be846566ee89f52adcfcfbfd19a3a481b9f
Bruno Martin <bruno.martin@hacklab.com.br>
2016-01-16 06:02:40 -0200

    refatorado django admin

--- mapaguarani/core/admin.py
+++ mapaguarani/core/admin.py
@@ -7,8 +7,6 @@ from .models import (
     ArchaeologicalPlace, ArchaeologicalImage, LandTenure, LandTenureStatus
 )
 from moderation.admin import ModerationAdmin
-# from django.contrib.gis.maps.google import GoogleMap
-# GMAP = GoogleMap(key='AIzaSyDX4lCrBqaA-sXJTsm6NQhERpua6p-9SIQ') # Can also set GOOGLE_MAPS_API_KEY in settings
 
 
 class PopulationInLine(admin.TabularInline):
@@ -19,17 +17,28 @@ class GuaraniPresenceInLine(admin.TabularInline):
     model = GuaraniPresence
 
 
+class IndigenousPlaceAdmin(geoadmin.GeoModelAdmin, ModerationAdmin):
+    map_template = 'openlayers.html'
+    list_per_page = 500
+
+    def get_ethnic_groups(self, obj):
+        return ", ".join([ethnic_group.name for ethnic_group in obj.ethnic_groups.all()])
+    get_ethnic_groups.short_description = _('Ethnic Group')
+
+    def get_prominent_subgroup(self, obj):
+        return ", ".join([prominent_sub.name for prominent_sub in obj.prominent_subgroup.all()])
+    get_prominent_subgroup.short_description = _('Prominent Ethnic SubGroup')
+
+
 @admin.register(IndigenousVillage)
-class IndigenousVillageAdmin(geoadmin.GeoModelAdmin):
+class IndigenousVillageAdmin(IndigenousPlaceAdmin):
     # extra_js = [GMAP.api_url + GMAP.key]
     # map_template = 'gis/admin/google.html'
-    map_template = 'openlayers.html'
     list_display = ('name', 'other_names', 'get_ethnic_groups', 'get_prominent_subgroup',
                     'population', 'get_guarani_presence',
                     'position_precision', 'position_source', 'geometry',
                     'public_comments', 'private_comments',)
     search_fields = ['name', 'other_names', ]
-    list_per_page = 500
     inlines = [
         PopulationInLine,
         GuaraniPresenceInLine,
@@ -40,30 +49,15 @@ class IndigenousVillageAdmin(geoadmin.GeoModelAdmin):
     get_guarani_presence.short_description = _('Guarani presence')
     get_guarani_presence.boolean = True
 
-    def get_ethnic_groups(self, obj):
-        return "\n".join([ethnic_group.name for ethnic_group in obj.ethnic_groups.all()])
-    get_ethnic_groups.short_description = _('Ethnic Group')
-
-    def get_prominent_subgroup(self, obj):
-        return "\n".join([prominent_sub.name for prominent_sub in obj.prominent_subgroup.all()])
-    get_prominent_subgroup.short_description = _('Prominent Ethnic SubGroup')
-
 
 @admin.register(IndigenousLand)
-class IndigenousLandAdmin(geoadmin.GeoModelAdmin, ModerationAdmin):
-    # display_wkt = True
-    map_template = 'openlayers.html'
+class IndigenousLandAdmin(IndigenousPlaceAdmin):
     list_display = ('name', 'other_names', 'get_prominent_subgroup', 'official_area', 'claim', 'demand', 'source',
                     'land_tenure', 'land_tenure_status', 'public_comments', 'private_comments', 'associated_land')
     # list_editable = ('other_names', 'official_area', 'claim', 'demand', 'source', 'land_tenure',
     #                  'land_tenure_status', 'public_comments', 'private_comments')
     search_fields = ['name', 'other_names', 'claim', 'demand', 'source', 'land_tenure__name',
                      'land_tenure_status__name', 'public_comments', 'private_comments']
-    list_per_page = 500
-
-    def get_prominent_subgroup(self, obj):
-        return "\n".join([prominent_sub.name for prominent_sub in obj.prominent_subgroup.all()])
-    get_prominent_subgroup.short_description = _('Prominent Ethnic SubGroup')
 
 
 class ArchaeologicalImageInLine(admin.TabularInline):
